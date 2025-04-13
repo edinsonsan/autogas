c03b0570a64f554cdb260a280370f35b098bc8b4
Edinson Sandoval <sandovalescobaredinson151@gmail.com>
2025-04-13 06:07:58 -0500

    Creando algunos Widgets Perzonalizados

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 // import 'package:autogas/src/presentation/page/auth/login/login_content.dart';
 import 'package:autogas/src/config/theme/app_theme.dart';
-import 'package:autogas/src/presentation/page/auth/forgot_password/forgot_password.dart';
+// import 'package:autogas/src/presentation/page/auth/forgot_password/forgot_password.dart';
+import 'package:autogas/src/presentation/page/auth/login/login_content.dart';
 // import 'package:autogas/src/presentation/page/auth/login/login_content.dart';
 // import 'package:autogas/src/presentation/page/auth/register/register_content.dart';
 import 'package:flutter/material.dart';
@@ -16,7 +17,7 @@ class MyApp extends StatelessWidget {
       theme: AppTheme(isDarkmode: false).getTheme(),
       debugShowCheckedModeBanner: false,
       title: 'Material App',
-      home: const ForgotPassword(),
+      home: const LoginContent(),
     );
   }
 }
